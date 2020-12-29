3ac046adf5ea7b724b5819032c555f38932560fd
memsss <aggelos.bilai73@gmail.com>
2020-12-29 22:43:19 +0200

    MatrixV.kt

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/MatrixV.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/MatrixV.kt
@@ -7,6 +7,7 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.ClientUtils
 import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
 
 @ModuleInfo(
     name = "MatrixV",
@@ -18,17 +19,18 @@ class MatrixV: Module() {
     private val allowValue = BoolValue("Allow?", false)
     private val lowerValue = BoolValue("Lower?", false)
     private val sameValue = BoolValue("SameY?", false)
+    private val lowValue = FloatValue("LowValue!!", 1.0F, 0.0F, 10.0F)
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         if(allowValue.get()) {
             if(lowerValue.get()) {
                 if(sameValue.get()) {
-                    mc.thePlayer!!.posY -= 0.1
-                    ClientUtils.displayChatMessage("posY NOW! ${mc.thePlayer!!.posY}")
+                    mc.thePlayer!!.setPosition(mc.thePlayer!!.posX, mc.thePlayer!!.posY + lowValue.get(), mc.thePlayer!!.posZ)
+                    ClientUtils.displayChatMessage("posY NOW! &4${mc.thePlayer!!.posY}")
                 } else {
-                    mc.thePlayer!!.posY = 0.1
-                    ClientUtils.displayChatMessage("posY NOW! ${mc.thePlayer!!.posY}")
+                    mc.netHandler.addToSendQueue(classProvider.createCPacketPlayerPosition(mc.thePlayer!!.posX, lowValue.get().toDouble(), mc.thePlayer!!.posZ, true))
+                    ClientUtils.displayChatMessage("posY NOW! &7${mc.thePlayer!!.posY}")
                 }
             }
         } else return

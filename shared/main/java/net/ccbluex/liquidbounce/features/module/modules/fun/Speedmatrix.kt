7b5b32b4c19167f0554e1520710abb6d63a2d968
memsss <aggelos.bilai73@gmail.com>
2020-12-31 17:39:31 +0200

    speedboi

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Speedmatrix.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Speedmatrix.kt
@@ -23,25 +23,25 @@ class Speedmatrix: Module() {
     private val timerValue = FloatValue("Timer", 1F, 0.1F, 2F)
     private val speedAir2 = FloatValue("SpeedInAir2", 0.02F, 0.00F, 1F)
     private val timerValue2 = FloatValue("Timer2", 1F, 0.1F, 2F)
-    private val motionY = FloatValue("MotionY", 0.42f, 0.00f, 1f)
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         if(allowValue.get()) {
-            if(!mc.thePlayer!!.onGround) {
-                if(mc.thePlayer!!.fallDistance > fallValue.get()) {
-                    mc.thePlayer!!.speedInAir = speedAir.get()
-                    mc.timer.timerSpeed = timerValue.get()
-                }
-            } else {
-                if(MovementUtils.isMoving) {
-                    mc.thePlayer!!.sprinting = true
-                    mc.thePlayer!!.motionY = motionY.get().toDouble()
-                    mc.thePlayer!!.jump()
-                    mc.thePlayer!!.speedInAir = speedAir2.get()
-                    mc.timer.timerSpeed = timerValue2.get()
+            if(mc.thePlayer!!.isInWater) {
+                if (!mc.thePlayer!!.onGround) {
+                    if (mc.thePlayer!!.fallDistance > fallValue.get()) {
+                        mc.thePlayer!!.speedInAir = speedAir.get()
+                        mc.timer.timerSpeed = timerValue.get()
+                    }
                 } else {
-                    mc.timer.timerSpeed = 1f
+                    if (MovementUtils.isMoving) {
+                        mc.thePlayer!!.sprinting = true
+                        mc.thePlayer!!.jump()
+                        mc.thePlayer!!.speedInAir = speedAir2.get()
+                        mc.timer.timerSpeed = timerValue2.get()
+                    } else {
+                        mc.timer.timerSpeed = 1f
+                    }
                 }
             }
         } else return
@@ -49,6 +49,5 @@ class Speedmatrix: Module() {
     override fun onDisable() {
         mc.timer.timerSpeed = 1f
         mc.thePlayer!!.speedInAir = 0.02f
-        mc.thePlayer!!.motionY = 0.42
     }
 }

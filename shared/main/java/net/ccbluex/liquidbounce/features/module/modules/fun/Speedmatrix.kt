70dc7469f65f12d908e51ad3cee85866e970e2a6
memsss <aggelos.bilai73@gmail.com>
2021-01-03 01:40:33 +0200

    speedboi

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Speedmatrix.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Speedmatrix.kt
@@ -20,7 +20,7 @@ import net.ccbluex.liquidbounce.value.IntegerValue
 class Speedmatrix: Module() {
 
     private val allowValue = BoolValue("Allow", false)
-    private val fallValue = FloatValue("FallDist", 1F, 0.1F, 2F)
+    //private val fallValue = FloatValue("FallDist", 1F, 0.1F, 2F)
     private val speedAir = FloatValue("SpeedInAir", 0.02F, 0.00F, 1F)
     private val timerValue = FloatValue("Timer", 1F, 0.1F, 2F)
     private val speedAir2 = FloatValue("SpeedInAir2", 0.02F, 0.00F, 1F)
@@ -47,8 +47,8 @@ class Speedmatrix: Module() {
                     if(pass1) {
                         passTimer.update()
                         if(passTimer.hasTimePassed(tickValue.get())) {
-                            mc.timer.timerSpeed = timerValue.get()
-                            mc.thePlayer!!.speedInAir = speedAir.get()
+                            mc.timer.timerSpeed = timerValue2.get()
+                            mc.thePlayer!!.speedInAir = speedAir2.get()
                             pass1 = false
                             passTimer.reset()
                         }

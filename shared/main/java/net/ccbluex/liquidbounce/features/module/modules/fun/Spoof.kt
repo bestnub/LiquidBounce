91e3fa0ea9f5a8a43dec0a52e08f4036c7f6438e
memsss <aggelos.bilai73@gmail.com>
2021-01-05 20:13:35 +0200

    spoof

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Spoof.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Spoof.kt
@@ -5,10 +5,10 @@ import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.features.module.modules.world.FastPlace
-import net.ccbluex.liquidbounce.value.BoolValue
-import net.ccbluex.liquidbounce.value.IntegerValue
-import kotlin.math.floor
+import net.ccbluex.liquidbounce.utils.ClientUtils
+import net.ccbluex.liquidbounce.utils.InventoryUtils
+import net.ccbluex.liquidbounce.utils.timer.TickTimer
+import net.ccbluex.liquidbounce.value.*
 
 @ModuleInfo(
     name = "Spoof",
@@ -18,43 +18,63 @@ import kotlin.math.floor
 
 class Spoof: Module() {
 
-    private var hotBar = IntegerValue("Slot", 9, 1, 9)
-    private var onUse = BoolValue("OnUse", false)
-    private var swing = BoolValue("Swing", true)
-    private var randomSlot = BoolValue("RandomSlot", false)
-    var delay = IntegerValue("Delay", 10, 0, 50)
+    private val allowValue = BoolValue("Allow", false)
+    private val allowValue2 = BoolValue("Allow2", false)
+    private val allowValue3 = BoolValue("Allow3", false)
+    private val allowValue4 = BoolValue("Allow4", false)
+    private val timerValue = IntegerValue("Ticks", 0, 0, 20)
+    private val debugValue = BoolValue("Debug", false)
+    private val debugTicks = IntegerValue("Debug-Ticks", 0, 0, 20)
 
-    private var tick = 0
-    @EventTarget
-    fun onUpdate(event: UpdateEvent) {
-        tick++
-        if(((mc.gameSettings.keyBindUseItem.pressed && onUse.get()) || !onUse.get())) {
-            if(swing.get() && mc.gameSettings.keyBindUseItem.pressed) {
-                mc.thePlayer!!.swingItem()
-            }
-            if(!randomSlot.get()) {
-                if(tick >= delay.get()) {
-                    mc.thePlayer!!.sendQueue.addToSendQueue(classProvider.createCPacketHeldItemChange(hotBar.get() - 1))
-                    tick = 0
-                }
-            } else {
-                if(tick >= delay.get()) {
-                    mc.thePlayer!!.sendQueue.addToSendQueue(classProvider.createCPacketHeldItemChange(randomIntFrom(0,8).toInt()))
-                    tick = 0
-                }
-            }
-        }
-    }
-
-    override fun onDisable() {
-        mc.rightClickDelayTimer = 4
-    }
+    private var allowTick = TickTimer()
+    private var debugTick= TickTimer()
+    private val blockSlot = InventoryUtils.findAutoBlockBlock()
+    private var slot = 0
 
     override fun onEnable() {
-        mc.rightClickDelayTimer = FastPlace().speedValue.get()
+        slot = mc.thePlayer!!.inventory.currentItem
     }
-    private fun randomIntFrom(min: Int, max: Int): Double// Get a random integer from [min] to [max]
-    {
-        return floor(Math.random()*(max-min+1)+min)
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        if (blockSlot == -1)
+            return
+
+        if(debugValue.get()) {
+            debugTick.update()
+            if(debugTick.hasTimePassed(debugTicks.get())) {
+                ClientUtils.displayChatMessage("Block slot: ${blockSlot}")
+                debugTick.reset()
+            }
+        } else return
+        if(allowValue.get()) {
+            if(mc.thePlayer!!.inventory.currentItem != slot) {
+                mc.thePlayer!!.inventory.currentItem = slot
+            } else return
+        } else return
+        if(allowValue2.get()) {
+            if(blockSlot - 36 != slot) {
+                mc.netHandler.addToSendQueue(classProvider.createCPacketHeldItemChange(blockSlot - 36))
+            } else return
+        } else return
+        if(allowValue3.get()) {
+            if(blockSlot >= 0) {
+                mc.thePlayer!!.inventory.currentItem = blockSlot - 36
+                mc.playerController.updateController()
+            } else return
+        } else return
+        if(allowValue4.get()) {
+            mc.netHandler.addToSendQueue(classProvider.createCPacketHeldItemChange(blockSlot - 36))
+        } else return
+
+        if(mc.gameSettings.keyBindUseItem.pressed) {
+            if(allowValue4.get()) {
+                allowTick.update()
+                if(allowTick.hasTimePassed(timerValue.get())) {
+                    mc.netHandler.addToSendQueue(classProvider.createCPacketHeldItemChange(slot))
+                    allowTick.reset()
+                }
+            } else return
+        }
     }
 }
